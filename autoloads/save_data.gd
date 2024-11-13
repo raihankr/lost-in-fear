cd95d38594fb94a104411e8a772cd7165df32548
Raihan Khairul Rochman <rkhairulrochman912@gmail.com>
2024-11-13 14:05:05 +0700

    Add SaveData autoloads

--- autoloads/save_data.gd
+++ autoloads/save_data.gd
@@ -1,10 +1,30 @@
-extends Object
+extends Node
 
-var x: int = 1
+var save_name: String = '.default'
+var data: Dictionary = {}
 
 func _notification(what):
-	if what ==:
-		pass
+	if what == NOTIFICATION_WM_CLOSE_REQUEST:
+		save_data(save_name)
+		print('Succesfully saved game states and exitting...')
 
-func save_data():
-	pass
+func _create_save_dir():
+	var dir: DirAccess = DirAccess.open('user://')
+	if not dir.dir_exists('savedata'):
+		dir.make_dir('savedata')
+
+func save_data(path: String = save_name) -> void:
+	_create_save_dir()
+	var file: FileAccess = FileAccess.open(path, FileAccess.WRITE)
+	file.store_var(data)
+	file.close()
+
+func load_data(path: String) -> Dictionary:
+	_create_save_dir()
+	var file: FileAccess = FileAccess.open(path, FileAccess.READ)
+	var _data  = file.get_var()
+	file.close()
+	return _data
+
+func load_and_store_data(path: String) -> void:
+	data = load_data(path)
