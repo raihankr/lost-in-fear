463e8c04c7acc0b20d11b8071f852f2d5c7f9b9c
Raihan Khairul Rochman <rkhairulrochman912@gmail.com>
2024-11-19 12:32:16 +0700

    Bug fixes

--- scripts/entities/items/flashlight.gd
+++ scripts/entities/items/flashlight.gd
@@ -1,6 +1,6 @@
 extends Item
 
 func pick_up(save_to_inventory: bool = true):
-	var player: Player = SceneManager.player
+	var player: Player = get_tree().current_scene.player
 	player.vision_style = player.VisionStyle.CONE
 	queue_free()
