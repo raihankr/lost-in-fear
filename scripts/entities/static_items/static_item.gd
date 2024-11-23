548f377c28108028de03bcbf44cea06dddd30e1f
Raihan Khairul Rochman <rkhairulrochman912@gmail.com>
2024-11-23 17:55:08 +0700

    Updates

--- scripts/entities/static_items/static_item.gd
+++ scripts/entities/static_items/static_item.gd
@@ -2,10 +2,13 @@ class_name StaticItem extends Area2D
 
 const OUTLINE_MATERIAL = preload("res://resources/materials/white_outline.tres")
 
+@export var subview: Texture = null
+
 @onready var texture_node: Sprite2D = $Texture
 
 func interact():
-	pass
+	if subview:
+		Global.show_image_subview(subview)
 
 func _on_area_entered(area: Area2D):
 	if area.is_in_group('action_area'):
